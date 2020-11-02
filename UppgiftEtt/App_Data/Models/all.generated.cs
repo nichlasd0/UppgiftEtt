3c18c3b11421d1c09554b9028e6dd52af420d66c
Nichlas <nichlasd@gmail.com>
2020-11-02 14:33:37 +0100

    add form to contact-us page

--- UppgiftEtt/App_Data/Models/all.generated.cs
+++ UppgiftEtt/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "62121019c0a102b0")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 
 // FILE: models.generated.cs
